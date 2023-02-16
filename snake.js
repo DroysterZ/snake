ccf161d88770bfbc8b2f3355b280e50170c529f2
Arthur Lopes <alopes@fivecom.com.br>
2023-02-16 16:53:53 -0300

    pathfinder funcionando

--- snake.js
+++ snake.js
@@ -16,7 +16,7 @@ let gameId = null;
 // Grid e jogo
 let size = 20;
 let grid = 20;
-let speed = 200;
+let speed = 100;
 let initialSize = 1;
 
 let fontSize = 10;
@@ -58,13 +58,15 @@ function startGame() {
 }
 
 function process() {
+	// Reinicia as globais
+	canMove = true;
+
 	// Processamento da cobrinha
 	snakeProcess();
 
 	// Atualiza a posição da cobrinha
 	updateSnake();
 
-	
 	buildMap();
 
 	// Verifica se a cobrinha colidiu com a borda ou consigo mesma
@@ -79,48 +81,36 @@ function process() {
 
 // Gera a matriz do mapa
 function buildMap() {
-	matrixMap = [];
+	// matrixMap = [];
 	for (x = 0; x < grid; x++) {
 		matrixMap[x] = [];
 		for (y = 0; y < grid; y++) {
+			matrixMap[x][y] = {};
 			let neighbors = [];
-
 			if (x % 2 == 0) {
-				if (y - 1 >= 0) {
-					let valid = true;
-					for (let i = 1; i < snake.length; i++) {
-						if (y - 1 == snake[i].y) valid = false;
-					}
-					if (valid) neighbors.push({ x: x, y: y - 1 });
-				}
+				neighbors.push({ x: x, y: y - 1 });
 			} else {
-				if (y + 1 < grid) {
-					let valid = true;
-					for (let i = 1; i < snake.length; i++) {
-						if (y + 1 == snake[i].y) valid = false;
-					}
-					if (valid) neighbors.push({ x: x, y: y + 1 });
-				}
+				neighbors.push({ x: x, y: y + 1 });
 			}
 
 			if (y % 2 == 0) {
-				if (x + 1 < grid) {
-					let valid = true;
-					for (let i = 1; i < snake.length; i++) {
-						if (x + 1 == snake[i].x) valid = false;
-					}
-					if (valid) neighbors.push({ x: x + 1, y: y });
-				}
+				neighbors.push({ x: x + 1, y: y });
 			} else {
-				if (x - 1 >= 0) {
-					let valid = true;
-					for (let i = 1; i < snake.length; i++) {
-						if (x - 1 == snake[i].x) valid = false;
-					}
-					if (valid) neighbors.push({ x: x - 1, y: y });
-				}
+				neighbors.push({ x: x - 1, y: y });
 			}
 
+			let toRemove = [];
+			for (let i = 0; i < neighbors.length; i++) {
+				let col = checkCollision(neighbors[i].x, neighbors[i].y);
+				if (col.collision && col.obj == 'wall') {
+					toRemove.push(i);
+				}
+			}
+			toRemove.reverse();
+			for (let i = 0; i < toRemove.length; i++) {
+				neighbors.splice(toRemove[i], 1);
+			}
+			
 			matrixMap[x][y] = { neighbors: neighbors };
 		}
 	}
@@ -162,7 +152,10 @@ function updateSnake() {
 	if (direction == 'l') snake.unshift({ x: x - 1, y: y });
 	if (direction == 'r') snake.unshift({ x: x + 1, y: y });
 
-	let ret = checkCollision();
+	x = snake[0].x;
+	y = snake[0].y;
+
+	let ret = checkCollision(x, y);
 	if (!ret.collision) {
 		snake.pop();
 	} else {
@@ -174,9 +167,7 @@ function updateSnake() {
 	}
 }
 
-function checkCollision() {
-	let x = snake[0].x;
-	let y = snake[0].y;
+function checkCollision(x, y) {
 	let ret = { collision: false, obj: null };
 	if (x == food.x && y == food.y) {
 		score++;
@@ -212,13 +203,16 @@ function gameOver() {
 function snakeProcess() {
 	path = findPath(snake[0], food);
 	if (path == null) path = findPath(snake[0], snake.at(-1));
+	// if (path == null) path = analyze();
 
 	let nextMove = path[0];
 
-	if (nextMove.x > snake[0].x) direction = 'r'
-	else if (nextMove.x < snake[0].x) direction = 'l'
-	else if (nextMove.y > snake[0].y) direction = 'u'
-	else if (nextMove.y < snake[0].y) direction = 'd'
+	let nextDirection = '';
+	if (nextMove.x > snake[0].x) nextDirection = 'r'
+	else if (nextMove.x < snake[0].x) nextDirection = 'l'
+	else if (nextMove.y > snake[0].y) nextDirection = 'u'
+	else if (nextMove.y < snake[0].y) nextDirection = 'd'
+	updateMove(nextDirection);
 }
 
 function heuristic(orig, dest) {
@@ -271,6 +265,86 @@ function findPath(start, end) {
 	}
 
 	return null; // retorna null se não encontrar um caminho
+}
+
+// Atualiza a direcao de movimento
+function updateMove(input) {
+	if (canMove) {
+		let reverses = [];
+		reverses['u'] = 'd';
+		reverses['d'] = 'u';
+		reverses['l'] = 'r';
+		reverses['r'] = 'l';
+
+		if (input != reverses[direction]) {
+			direction = input;
+			canMove = false;
+		}
+	}
+}
+
+function analyze() {
+	let x = snake[0].x;
+	let y = snake[0].y;
+
+	let marginMin = 0;
+	let marginMax = grid - 1;
+
+	let options = matrixMap[x][y].neighbors;
+	/*
+	for (let i = 0; i < options.length; i++) {
+		if (options[i].x > x) direction = 'r'
+		else if (options[i].x < x) direction = 'l'
+		else if (options[i].y > y) direction = 'u'
+		else if (options[i].y < y) direction = 'd'
+	}
+	*/
+	/*
+	let toRemove = [];
+	for (let i = 0; i < options.length; i++) {
+		let chance = options[i];
+	
+		// Verifica se o proximo movimento vai bater na parede
+		if (chance.x < marginMin || chance.y < marginMin || chance.x > marginMax || chance.y > marginMax) {
+			toRemove.push(i);
+		}
+	
+		// Verifica se o proximo movimento vai bater na cobra
+		for (let j = 0; j < snake.length; j++) {
+			if (chance.x == snake[j].x && chance.y == snake[j].y) {
+				toRemove.push(i);
+			}
+		}
+	}
+	
+	toRemove = toRemove.filter((elem, index) => toRemove.indexOf(elem) === index);
+	toRemove.sort(function (a, b) { return b - a });
+	
+	for (let i = 0; i < toRemove.length; i++) {
+		options.splice(toRemove[i], 1);
+	}
+	
+	let minDist = grid + grid;
+	let chosen = {};
+	let minDistOptions = [];
+	
+	for (let i = 0; i < options.length; i++) {
+		let chance = options[i];
+		if (matrixDistance[chance.x][chance.y] < minDist) {
+			minDistOptions = [chance];
+			minDist = matrixDistance[chance.x][chance.y];
+		} else if (matrixDistance[chance.x][chance.y] == minDist) {
+			minDistOptions.push(chance);
+		}
+	}
+	*/
+	if (options.length > 1) {
+		chosen = options[Math.floor(Math.random() * (options.length))];
+	} else {
+		chosen = options[0];
+	}
+
+	return [chosen];
 }
 
 function draw() {
